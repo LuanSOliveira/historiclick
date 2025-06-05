de8737f67836cf65ea7e521096d98428f2cf9a11
Luan <l.u.a.n@hotmail.com>
2025-06-04 22:10:30 -0300

    fix: Ajuste de imagem de capa

--- src/shared/components/PageComponents/ImageNavigate/index.tsx
+++ src/shared/components/PageComponents/ImageNavigate/index.tsx
@@ -8,22 +8,24 @@ interface Props {
 
 const ImageNavigate = ({ img, setImageView }: Props) => {
   return (
-    <div className="w-full h-full flex flex-col gap-5">
+    <div className="w-full h-full flex flex-col gap-5 items-center">
       <img
         src={img}
         alt="Imagem da história"
-        className="border-3 border-paleta-2 h-120"
+        className="border-3 border-paleta-2 h-120 w-100 lg:h-170 lg:w-140"
       />
-      <Button
-        variant="outlined"
-        endIcon={<SendIcon />}
-        color="inherit"
-        fullWidth
-        size="small"
-        onClick={() => setImageView(false)}
-      >
-        CONTINUAR
-      </Button>
+      <div className="w-full lg:w-140">
+        <Button
+          variant="outlined"
+          endIcon={<SendIcon />}
+          color="inherit"
+          fullWidth
+          size="small"
+          onClick={() => setImageView(false)}
+        >
+          CONTINUAR
+        </Button>
+      </div>
     </div>
   );
 };
